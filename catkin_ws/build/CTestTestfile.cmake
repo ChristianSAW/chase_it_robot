125c88dd8ca06bcc474c9375e73a21aaa71de7f6
Christian Welling <csw73@cornell.edu>
2020-06-22 21:32:55 +0000

    .gitignore fix

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/chase_it_robot/catkin_ws/src
-# Build directory: /home/workspace/chase_it_robot/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(my_robot)
